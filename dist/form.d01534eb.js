70bf6487e70b1980465a514e0b5934c6064823ec
soer145a <soren.remboll@gmail.com>
2019-05-24 13:43:46 +0100

    Form func done

--- dist/form.d01534eb.js
+++ dist/form.d01534eb.js
@@ -118,7 +118,152 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"form.js":[function(require,module,exports) {
+"use strict";
 
+window.addEventListener("DOMContentLoaded", init);
+var userArray = [];
+var front = document.querySelector("#formLogin");
+var userData = document.querySelector("#formUserBlock");
+var userInfo = document.querySelector("#formUserInfo");
+
+function init() {
+  fetchMyJson();
+  console.log("init");
+}
+
+function fetchMyJson() {
+  fetch("https://examusers-4b00.restdb.io/rest/databaseuser?key=22631469345172666884", {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "x-apikey": "5cdbfbb9f66d7b1062cb34b7"
+    }
+  }).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    userArray = data;
+    console.log(userArray);
+    document.querySelector("#formUserButton").addEventListener("click", displayFirstForm);
+    document.querySelector("#formLoginButton").addEventListener("click", function () {
+      checkLoginStatus();
+    });
+  });
+}
+
+function checkLoginStatus() {
+  console.log("CHECK LOGIN");
+  var formUserName = document.querySelector("#formUsername").value;
+  var formPassword = document.querySelector("#formPassword").value;
+  console.log(formUserName);
+  userArray.forEach(function (user) {
+    if (formUserName == user.username) {
+      if (user.password == formPassword) {
+        document.querySelector("body").style.backgroundColor = "blue";
+      }
+    }
+  });
+}
+
+function displayFirstForm() {
+  front.style.display = "none";
+  userData.style.display = "block";
+  var dataObject = {
+    username: "-placeholder-",
+    password: "-placeholder-",
+    email: "-placeholder-",
+    telefoneNr: "-placeholder-",
+    fornavn: "-placeholder-",
+    efternavn: "-placeholder-",
+    adresse: "-placeholder-",
+    userID: "-placeholder-",
+    city: "-placeholder-",
+    CPRnr: "-placeholder-"
+  };
+  document.querySelector("#formUserBlockFormButton").addEventListener("click", function () {
+    console.log("CLIKED BUTTON");
+    saveFirstSetOfData(dataObject);
+  });
+}
+
+function saveFirstSetOfData(obj) {
+  console.log("SAVE FIRST SET OF DATA");
+  obj.username = document.querySelector("#userName").value;
+  var userCounter = 0;
+  userArray.forEach(function (user) {
+    console.log(user.username, obj.username);
+
+    if (user.username == obj.username) {
+      alert("OPTAGET BRUGERNAVN");
+    } else {
+      if (document.querySelector("#userPassword").value == document.querySelector("#userPasswordConfirm").value) {
+        obj.password = document.querySelector("#userPassword").value;
+
+        if (user.email == obj.email) {
+          alert("OPTAGET EMAIL");
+        } else {
+          obj.email = document.querySelector("#userEmail").value;
+
+          if (obj.telefoneNr == user.telefoneNr) {
+            alert("OPTAGET TELEFONNR");
+          } else {
+            obj.telefoneNr = document.querySelector("#userTlf").value;
+            userCounter++;
+            obj.userID = Math.random().toString(36).substr(2, 9);
+          }
+        }
+      } else {
+        alert("Password1 og password 2 er ikke ens");
+      }
+    }
+  });
+
+  if (userCounter == userArray.length) {
+    console.log("CORRECT");
+    displaySecondForm(obj);
+  } else {
+    alert("Something Was Wrong");
+  }
+}
+
+function displaySecondForm(obj) {
+  console.log("DISPLAY2");
+  userData.style.display = "none";
+  userInfo.style.display = "block";
+  document.querySelector("#formUserInfoButton").addEventListener("click", function () {
+    saveSecondSetOfData(obj);
+  });
+}
+
+function saveSecondSetOfData(obj) {
+  obj.fornavn = document.querySelector("#userFirstName").value;
+  obj.efternavn = document.querySelector("#userLastName").value;
+  obj.adresse = document.querySelector("#userAdress").value;
+  obj.city = document.querySelector("#userCity").value;
+  userArray.forEach(function (user) {
+    if (user.CPRnr == obj.CPRnr) {
+      alert("Dette CPR nummer brugt før");
+    } else {
+      obj.CPRnr = document.querySelector("#userCpr").value;
+      sendInfoToRest(obj);
+    }
+  });
+}
+
+function sendInfoToRest(obj) {
+  front.style.display = "block";
+  userInfo.style.display = "none";
+  var postData = JSON.stringify(obj);
+  fetch("https://examusers-4b00.restdb.io/rest/databaseuser?key=22631469345172666884", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "5cdbfbb9f66d7b1062cb34b7",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  });
+}
 },{}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +292,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57147" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59361" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

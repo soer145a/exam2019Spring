a6a152302ad37f6b9c6e82801dcb761aadaedcfc
soer145a <soren.remboll@gmail.com>
2019-05-31 11:04:57 +0100

    spil

--- dist/output.c16d2514.js
+++ dist/output.c16d2514.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52847" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63988" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
